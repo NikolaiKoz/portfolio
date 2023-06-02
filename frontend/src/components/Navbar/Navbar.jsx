34f86b81b2d1f835d235af381c107acdfd6af5d8
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-06-02 19:47:41 -0300

    feat: :art: Reset CSS was created.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
+import React from 'react';
+import styles from './Navbar.module.scss';
+import { FcMenu } from 'react-icons/fc';
+import '../../assets/logo/Logo.png';
+
 function Navbar() {
 	return (
-		<>
+		<nav>
+			<img src='../../assets/logo/Logo.png' alt='' />
 			<div>HOla esto funciona </div>
-		</>
+		</nav>
 	);
 }
 
