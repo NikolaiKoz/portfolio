ffb6934b75ae869dbad24debdd60f475a8fc7a46
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-07-20 12:26:36 -0300

    feat: :sparkles: Se agrego el video de background.

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -9,18 +9,34 @@ import Experience from './Experience/Experience';
 import Contact from './Contact/Contact';
 import SocialMedia from './SocialMedia/SocialMedia';
 import CV from './CV/CV';
+import background from '../assets/videos/BackgroundNeon.mp4';
 
 function App() {
 	return (
-		<div className='wrapper'>
-			<Navbar />
-			<Sections SectionTitle='Sobre mí' SectionComponent={<About />} />
-			<Sections SectionTitle='Habilidades' SectionComponent={<Skills />} />
-			<Sections SectionTitle='Experiencia' SectionComponent={<Experience />} />
-			<Sections SectionTitle='Contacto' SectionComponent={<Contact />} />
-			<Sections SectionTitle='Redes Sociales' SectionComponent={<SocialMedia />} />
-			<Sections SectionTitle='CV' SectionComponent={<CV />} />
-		</div>
+		<>
+			<div className='wrapper'>
+				<Navbar />
+				<Sections SectionTitle='Sobre mí' SectionComponent={<About />} />
+				<Sections SectionTitle='Habilidades' SectionComponent={<Skills />} />
+				<Sections
+					SectionTitle='Experiencia'
+					SectionComponent={<Experience />}
+				/>
+				<Sections SectionTitle='Contacto' SectionComponent={<Contact />} />
+				<Sections
+					SectionTitle='Redes Sociales'
+					SectionComponent={<SocialMedia />}
+				/>
+				<Sections SectionTitle='CV' SectionComponent={<CV />} />
+			</div>
+			<video
+				className='videoBackground'
+				src={background}
+				autoPlay
+				loop
+				muted
+			></video>
+		</>
 	);
 }
 
