5286e2bc805c850c1ab466a6114467e13c34a894
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-07-20 20:19:18 -0300

    test: :art: Prueba con imagen de fondo.

--- frontend/src/components/VideoBackground/VideoBackground.jsx
+++ frontend/src/components/VideoBackground/VideoBackground.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-import { videoBackground } from './VideoBackground.module.scss';
+import { videoBackground, ImgBackgorund } from './VideoBackground.module.scss';
 import background from '../../assets/videos/BackgroundNeon.mp4';
+import backgroundImg from '../../assets/img/wall.jpg';
 
 
 function VideoBackground() {
   return (
-		<video
-			className={videoBackground}
-			src={background}
-			autoPlay
-			loop
-			muted
-			playsInline
-			loading='lazy'
-		></video>
+
+	<figure>
+		<img className={ImgBackgorund} src={backgroundImg} alt="FOndo prueba" />
+	</figure>
+		// <video
+		// 	className={videoBackground}
+		// 	src={background}
+		// 	autoPlay
+		// 	loop
+		// 	muted
+		// 	playsInline
+		// 	loading='lazy'
+		// ></video>
 	);
 }
 
