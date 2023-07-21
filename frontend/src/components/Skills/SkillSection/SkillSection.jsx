5cc9e3a4fecb8fe305007178403b3368e55b597f
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-07-21 20:45:52 -0300

    build: :construction: Skills WIP.

--- frontend/src/components/Skills/SkillSection/SkillSection.jsx
+++ frontend/src/components/Skills/SkillSection/SkillSection.jsx
@@ -5,9 +5,11 @@ import {
 	TitleIconContainer,
 	TitleIcon,
 	ContainerSkills,
+    Img,
+    ImgDescription
 } from './SkillSection.module.scss';
 import { FcExpand } from 'react-icons/fc';
-// import html5 from '../../../assets/icons/html5.svg';
+import html5 from '../../../assets/icons/html5.svg';
 // import css3 from '../../../assets/icons/css3.svg';
 // import javascript from '../../../assets/icons/javascript.svg';
 
@@ -21,7 +23,12 @@ function SkillSection() {
 				</span>
 			</div>
 			<div className={ContainerSkills}>
-                
+                <figure>
+                    <img className={Img} loading='lazy' src={html5} alt="Logo de HTML" />
+                </figure>
+                <figcaption className={ImgDescription}>
+                    HTML
+                </figcaption>
             </div>
 		</div>
 	);
