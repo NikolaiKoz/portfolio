53f8951b77e5b7d45a98d2fd25d7daf978f9552c
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-08-18 16:46:34 -0300

    build: :construction: Estructura HTML de aboutme.

--- app/src/pages/sections/aboutme/index.jsx
+++ app/src/pages/sections/aboutme/index.jsx
@@ -1,7 +1,16 @@
 export default function AboutMe () {
     return(
         <div>
-            <h1>Este es el AboutMe</h1>
+            <h2>Este es el AboutMe</h2>
+            <div>
+                <figure>
+                    <img src="" alt="Foto de perfil de Jose Luis Vallejos" loading="lazy" />
+                </figure>
+                <figcaption>
+                    <h3>Quien Soy y que hago?</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, asperiores illo accusamus sunt incidunt ab soluta quis totam maiores possimus cupiditate earum ex hic molestiae quas aliquam placeat consequatur eos!</p>
+                </figcaption>
+            </div>
         </div>
     )
 }
